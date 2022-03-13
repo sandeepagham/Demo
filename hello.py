0ea5bf27310052d00ea5c261ff55cecd248d170a
sandeepagham <sandeep.agham@gmail.com>
2022-03-13 10:16:34 +0530

    added check for file name and path

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 # This is for demo
 
+import os
 # this is new file
 print('Hello World!')
 print('Git Learning')
 print('Git with GitHub')
-print('this is file {}'.format(__name__))
+print('this is file name {}'.format(__name__))
+print('absolute path {}'.format(os.path.abspath(__file__)))
